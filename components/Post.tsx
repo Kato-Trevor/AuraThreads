947f2f7ffea9821524b516706a6d59b905f3775b
Kato-Trevor <katotrevor99@gmail.com>
2025-03-18 09:35:01 +0300

    fix: improved ui of the home page

--- components/Post.tsx
+++ components/Post.tsx
@@ -6,18 +6,18 @@ import Avatar from "./Avatar";
 const Post = ({ post }: { post: PostModel }) => {
   return (
     <Link
-        className="p-4 mb-4 bg-gray-100 rounded-lg flex-row posts-start"
-        href={{
-          pathname: "/threads/[id]",
-          params: { id: `${post.$id}` },
-        }}
-      >
-        <Avatar username={post.userId.username} className="mr-4"/>
-        <View className="ml-4 flex-1">
-          <Text className="text-gray-500">{post.userId.username}</Text>
-          <Text className="text-lg text-gray-800">{post.content}</Text>
-        </View>
-      </Link>
+      className="p-4 mb-4 bg-gray-100 rounded-lg flex-row items-start"
+      href={{
+        pathname: "/threads/[id]",
+        params: { id: `${post.$id}` },
+      }}
+    >
+      <Avatar username={post.userId.username} />
+      <View className="flex-1 pl-2">
+        <Text className="text-gray-500">{post.userId.username}</Text>
+        <Text className="text-lg text-gray-800 mt-1">{post.content}</Text>
+      </View>
+    </Link>
   );
 };
 
